113450f127c083e413b41fbbb988ad1b76549480
Octavian Pintiliciuc <o.pintiliciuc@kaizengaming.com>
2022-02-03 10:27:23 +0200

    Unit 1 WIP

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinTail
+{
+    class Messages
+    {
+        #region Neutral/System messages
+        /// <summary>
+        /// Marker class to continue processing
+        /// </summary>
+        public class ContinueProcessing { }
+        #endregion
+
+        #region Success messages
+        /// <summary>
+        /// Base class for signalling that user input was valid.
+        /// </summary>
+        public class InputSuccess
+        {
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+        #endregion
+
+        #region Error messages
+        public class InputError
+        {
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason) { }
+        }
+
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason) { }
+        }
+        #endregion
+    }
+}
